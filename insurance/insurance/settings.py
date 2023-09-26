39c4dec3375a0613149e30d8fc22394711f59738
kohlisaurabh <47265966+kohlisaurabhsagar@users.noreply.github.com>
2023-09-26 14:33:29 +0530

    ......

--- insurance/insurance/settings.py
+++ insurance/insurance/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-xb=r3sd_j8-@db_)ycn$5@_*&vygxx(%!tlnye!9u+y2!1%h@o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
